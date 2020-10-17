dc6e15d4f366b94a9fb93463453e7c744bd8977d
BINTEAM <58648810+binazure2299@users.noreply.github.com>
2020-10-17 11:00:59 +0700

    TRUONG01 - THÊM NÚT ĐĂNG BÀI
    TRUONG01 - THÊM NÚT ĐĂNG BÀI

--- blogadmin/header-user.php
+++ blogadmin/header-user.php
@@ -33,11 +33,11 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
           <ul class="nav navbar-nav side-nav">
-            <!-- <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li> -->
-            <!-- <li><a href="blogs_view.php"><i class="fa fa-rss"></i>Blogs</a></li> -->
-            <!-- <li><a href="blog_categories_view.php"><i class="fa fa-tags"></i>Categories</a></li> -->
-            <!-- <li><a href="blogs_view.php"><i class="fa fa-check"></i>Published</a></li> -->
-            <!-- <li><a href="#"><i class="fa fa-tasks"></i>Drafts</a></li> -->
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li><a href="blogs_view.php"><i class="fa fa-rss"></i>Blogs</a></li>
+            <li><a href="blog_categories_view.php"><i class="fa fa-tags"></i>Categories</a></li>
+            <li><a href="blogs_view.php"><i class="fa fa-check"></i>Published</a></li>
+            <li><a href="#"><i class="fa fa-tasks"></i>Drafts</a></li>
             <!-- c0000001 -->
             <?php
             $usernow=getLoggedMemberID();
@@ -51,53 +51,8 @@
           ?>
           </ul>
          <ul class="nav navbar-nav navbar-right navbar-user">
-            <li class="dropdown messages-dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> Messages <span class="badge">7</span> <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li class="dropdown-header">7 New Messages</li>
-                <li class="message-preview">
-                  <a href="#">
-                    <span class="avatar"><img src="http://placehold.it/50x50"></span>
-                    <span class="name">John Smith:</span>
-                    <span class="message">Hey there, I wanted to ask you something...</span>
-                    <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
-                  </a>
-                </li>
-                <li class="divider"></li>
-                <li class="message-preview">
-                  <a href="#">
-                    <span class="avatar"><img src="http://placehold.it/50x50"></span>
-                    <span class="name">John Smith:</span>
-                    <span class="message">Hey there, I wanted to ask you something...</span>
-                    <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
-                  </a>
-                </li>
-                <li class="divider"></li>
-                <li class="message-preview">
-                  <a href="#">
-                    <span class="avatar"><img src="http://placehold.it/50x50"></span>
-                    <span class="name">John Smith:</span>
-                    <span class="message">Hey there, I wanted to ask you something...</span>
-                    <span class="time"><i class="fa fa-clock-o"></i> 4:34 PM</span>
-                  </a>
-                </li>
-                <li class="divider"></li>
-                <li><a href="#">View Inbox <span class="badge">7</span></a></li>
-              </ul>
-            </li>
-            <li class="dropdown alerts-dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> Alerts <span class="badge">3</span> <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Default <span class="label label-default">Default</span></a></li>
-                <li><a href="#">Primary <span class="label label-primary">Primary</span></a></li>
-                <li><a href="#">Success <span class="label label-success">Success</span></a></li>
-                <li><a href="#">Info <span class="label label-info">Info</span></a></li>
-                <li><a href="#">Warning <span class="label label-warning">Warning</span></a></li>
-                <li><a href="#">Danger <span class="label label-danger">Danger</span></a></li>
-                <li class="divider"></li>
-                <li><a href="#">View All</a></li>
-              </ul>
-            </li>
+           
+           
             <li class="dropdown user-dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>  <?php echo getLoggedMemberID(); ?><b class="caret"></b></a>
             <ul class="dropdown-menu">
