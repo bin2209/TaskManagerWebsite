4d31d5bf73a0142f9b538af571e998a98324e0e2
BINTEAM <58648810+binazure2299@users.noreply.github.com>
2020-10-27 00:21:28 +0700

    GIAODIEN
    GIAODIEN

--- blogadmin/header-user.php
+++ blogadmin/header-user.php
@@ -27,17 +27,17 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Thành viên</a>
+          <!-- <a class="navbar-brand" href="index.php">Thành viên</a> -->
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
           <ul class="nav navbar-nav side-nav">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-            <li><a href="blogs_view.php"><i class="fa fa-rss"></i>Blogs</a></li>
-            <li><a href="blog_categories_view.php"><i class="fa fa-tags"></i>Categories</a></li>
-            <li><a href="blogs_view.php"><i class="fa fa-check"></i>Published</a></li>
-            <li><a href="#"><i class="fa fa-tasks"></i>Drafts</a></li>
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <!-- <li><a href="blogs_view.php"><i class="fa fa-rss"></i>Blogs</a></li> -->
+            <!-- <li><a href="blog_categories_view.php"><i class="fa fa-tags"></i>Categories</a></li> -->
+            <li><a href="blogs_view.php"><i class="fa fa-check"></i>Bảng công việc</a></li>
+            <!-- <li><a href="#"><i class="fa fa-tasks"></i>Drafts</a></li> -->
             <!-- c0000001 -->
             <?php
             $usernow=getLoggedMemberID();
@@ -89,7 +89,7 @@
   <div id="page-wrapper">
  <div class="row">
           <div class="col-lg-12">
-            <h1>Hello <?php echo getLoggedMemberID(); ?> !</h1>
+            <h1>Xin chào <?php echo getLoggedMemberID(); ?> !</h1>
             <ol class="breadcrumb">
               <li><a href="../index.php"><i class="icon-dashboard" style="text-decoration:none;"></i> <strong>View Website</strong></a></li>
               <li><a href="index.php"><i class="icon-dashboard" style="text-decoration:none;"></i> <strong>Dashboard</strong></a></li>
